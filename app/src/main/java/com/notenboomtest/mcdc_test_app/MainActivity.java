4d327c8e4f855567fec81baba36e3771604702d7
notenbed <edwin_notenboom@yahoo.com>
2016-09-11 11:04:11 +0200

    Check duplicate letters in sequence added

--- app/src/main/java/com/notenboomtest/mcdc_test_app/MainActivity.java
+++ app/src/main/java/com/notenboomtest/mcdc_test_app/MainActivity.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,18 +26,40 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String sequence = editText.getText().toString();
 
-        if (Pattern.matches("^[A-Za-z]{1}((\\||&)[A-Za-z])*$", sequence)){
+        if (!Pattern.matches("^[A-Za-z]{1}((\\||&)[A-Za-z])*$", sequence)) {
+            Context context = getApplicationContext();
+            CharSequence text = "The decision has to start with a letter and can be followed by one or more combinations of a | or & + letter. Please change";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+        else if (doubles(sequence)){
+            Context context = getApplicationContext();
+            CharSequence text = "The decision contains duplicate letters";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+
+        }
+        else {
             Intent intent = new Intent(this, TestcasesActivity.class);
             String message = editText.getText().toString();
             intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);}
-        else {
-            Context context = getApplicationContext();
-            CharSequence text = "The sequence doesn't met the form 'A&B|C'. Please change";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+            startActivity(intent);
         }
+
+    }
+    private boolean doubles(String sequence){
+        String[] temp = sequence.split("(\\||&)");
+        System.out.println(temp.length);
+        Set<String> list = new HashSet<>();
+        for (int i=0; i < temp.length;i++){
+            list.add(temp[i]);
+        }
+        System.out.println(list.size());
+        return !(list.size() == temp.length);
 
     }
 }
